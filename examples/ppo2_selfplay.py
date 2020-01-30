10a74e7a887a7f6fb3ef149cce418b08676106c3
Antti Neuvonen <anttine@student.uef.fi>
2020-01-30 14:14:03 +0200

    Update selfplay example

--- examples/ppo2_selfplay.py
+++ examples/ppo2_selfplay.py
@@ -1,19 +1,21 @@
 import os
 import random
-
+import time
+from glob import glob
+from pprint import pprint
+from datetime import datetime
+import argparse
 import tensorflow as tf
-
+from kuruve.envs.CompetitiveEnv import CompetitiveEnv
 from stable_baselines import PPO2
 from stable_baselines.common.policies import CnnPolicy
 from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
-
-from kuruve.envs.CompetitiveEnv import CompetitiveEnv
+from stable_baselines.common.evaluation import evaluate_policy
 
 # TODO: Not ready yet.
 
 import numpy as np
 
-import argparse
 
 # Arguments
 parser = argparse.ArgumentParser(description="Train selfplaying agent")
@@ -26,7 +28,8 @@ parser.add_argument("-timesteps", metavar="N", type=int, default=100000)
 parser.add_argument("-frameskip", metavar="N", type=int, default=10)
 parser.add_argument("-obs_size", metavar="N", type=int, default=96)
 parser.add_argument("-envs", metavar="N", type=int, default=1)
-parser.add_argument("-model", metavar="name", type=str, default="model_default")
+parser.add_argument("-model", metavar="name", type=str, default="model.pkl")
+parser.add_argument("-snapshot_time", type=int, default=21600, help="Time in seconds between saving snapshots (default: 3h)")
 
 parser.add_argument("-gpu", action="store_true", help="Enable gpu")
 
@@ -37,6 +40,8 @@ FRAMESKIP = args.frameskip
 MODEL_NAME = args.model
 TIMESTEPS = args.timesteps
 ENV_COUNT = args.envs
+
+SNAPSHOT_PREFIX = "model_snapshot"
 
 if args.gpu:
     # GPU Settings
@@ -52,22 +57,15 @@ else:
 
 # Credits to Torille
 
-print("Gpu settings -")
-config = tf.ConfigProto()
-config.gpu_options.allow_growth = True
-sess = tf.Session(config=config)
-print("Gpu settings ----")
-
-MODEL = "model_selfplay_6.zip"
-
 # no input, left, right
 possible_actions = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
 OBS_SIZE = (96, 96)
 FRAMESKIP = 10
 
+
 def create_env_headless():
-    if os.path.isfile(MODEL):
-        opponent_ppo = PPO2.load(MODEL)
+    if os.path.isfile(MODEL_NAME):
+        opponent_ppo = PPO2.load(MODEL_NAME)
 
         # Create assign ops for the oppoennt
         with opponent_ppo.graph.as_default():
@@ -87,7 +85,6 @@ def create_env_headless():
 
     def player2_step(obs):
         if not opponent_ppo.use_random_agent:
-            #obs = np.pad(obs[None], ((0, 3), (0, 0)), 'constant')
             action, opponent_ppo.hidden_state = opponent_ppo.predict(obs, state=opponent_ppo.hidden_state)
             return action
         else:
@@ -106,12 +103,16 @@ def create_env_headless():
                 # Load some previous model
                 print("---Loading old params---")
                 opponent_ppo.use_random_agent = False
-                load_file = MODEL
+                model_files = glob(SNAPSHOT_PREFIX + "*")
+                # If no snapshots available, load newest ones
+                load_file = MODEL_NAME
+                if len(model_files) > 0:
+                    load_file = random.choice(model_files)
                 opponent_ppo.load_parameters(load_file)
             else:
                 # Load most recent params
                 print("---Loading new params---")
-                opponent_ppo.load_parameters(MODEL)
+                opponent_ppo.load_parameters(MODEL_NAME)
                 opponent_ppo.use_random_agent = False
 
     return CompetitiveEnv(headless=True, observation_size=OBS_SIZE, fps_cap=0, frameskip=FRAMESKIP, enable_powerups=False,
@@ -120,8 +121,8 @@ def create_env_headless():
 
 # Lazy copy paste
 def create_env():
-    if os.path.isfile(MODEL):
-        opponent_ppo = PPO2.load(MODEL)
+    if os.path.isfile(MODEL_NAME):
+        opponent_ppo = PPO2.load(MODEL_NAME)
 
         # Create assign ops for the oppoennt
         with opponent_ppo.graph.as_default():
@@ -141,7 +142,6 @@ def create_env():
 
     def player2_step(obs):
         if not opponent_ppo.use_random_agent:
-            #obs = np.pad(obs[None], ((0, 3), (0, 0)), 'constant')
             action, opponent_ppo.hidden_state = opponent_ppo.predict(obs, state=opponent_ppo.hidden_state)
             return action
         else:
@@ -160,41 +160,75 @@ def create_env():
                 # Load some previous model
                 print("---Loading old params---")
                 opponent_ppo.use_random_agent = False
-                load_file = MODEL
+                model_files = glob(SNAPSHOT_PREFIX + "*")
+                # If no snapshots available, load newest ones
+                load_file = MODEL_NAME
+                if len(model_files) > 0:
+                    load_file = random.choice(model_files)
                 opponent_ppo.load_parameters(load_file)
             else:
                 # Load most recent params
                 print("---Loading new params---")
-                opponent_ppo.load_parameters(MODEL)
+                opponent_ppo.load_parameters(MODEL_NAME)
                 opponent_ppo.use_random_agent = False
 
-    return CompetitiveEnv(headless=False, observation_size=OBS_SIZE, fps_cap=60, frameskip=FRAMESKIP, enable_powerups=False,
+    return CompetitiveEnv(headless=False, observation_size=OBS_SIZE, fps_cap=0, frameskip=FRAMESKIP, enable_powerups=False,
                           verbose=1, player2_step=player2_step, player2_reset=player2_reset)
 
 
-def train(timesteps=10000, env_count=4):
-    envs = [create_env_headless for _ in range(env_count)]
+def train():
+    global last_snapshot_time
+    last_snapshot_time = time.time()
+
+    def callback(_locals, _globals):
+        global last_snapshot_time
+        # Save model
+        _locals['self'].save(MODEL_NAME)
+        # If enough time has passed, save snapshot
+        if (time.time() - last_snapshot_time) > args.snapshot_time:
+            timestamp = datetime.now()
+            filename = "{}_{}_{}_{}_{}.pkl".format(
+                SNAPSHOT_PREFIX, timestamp.day, timestamp.month, timestamp.hour, timestamp.minute
+            )
+            _locals['self'].save(filename)
+            last_snapshot_time = time.time()
+
+    envs = [create_env_headless for _ in range(ENV_COUNT)]
     vec_envs = SubprocVecEnv(envs, start_method="spawn")
 
     model = PPO2('CnnPolicy', vec_envs, verbose=1, ent_coef=0.00001, n_steps=256)
     #model = PPO2(CnnPolicy, vec_envs, verbose=1, n_steps=256)
 
-    if not os.path.isfile(MODEL):
-        model.save(MODEL)
+    if not os.path.isfile(MODEL_NAME):
+        model.save(MODEL_NAME)
         print("Run again to train")
+        vec_envs.close()
     else:
-        model.learn(total_timesteps=timesteps)
-        model.save(MODEL)
+        model.learn(total_timesteps=TIMESTEPS, callback=callback)
+        model.save(MODEL_NAME)
         print("Training Done")
+        vec_envs.close()
 
-    vec_envs.close()
+        # Evaluation
+        print("Evalution")
+        vec_env = create_env_headless()
+        vec_env = DummyVecEnv([lambda: vec_env])
+        model = PPO2.load(MODEL_NAME)
+        print("After Training evaluation")
+        print(evaluate_policy(model, vec_env, n_eval_episodes=100))
+        print(evaluate_policy(model, vec_env, n_eval_episodes=100))
+        vec_env.close()
+
+
+
+
 def play():
     vec_env = create_env()
     vec_env = DummyVecEnv([lambda: vec_env])
 
     # model = PPO2(CnnPolicy, vecEnv, verbose=1, ent_coef=0.0001, n_steps=256)
     # model.load("MODEL_cnn_5.zip", vecEnv)
-    model = PPO2.load(MODEL)
+    model = PPO2.load(MODEL_NAME)
     model.set_env(vec_env)
 
     game_count = 0
@@ -202,18 +236,8 @@ def play():
 
     t = 0
     while game_count < 10000:
-        #action = model.predict(obs)[0]
-        action = [0]
-        #print(obs.shape)
+        action = model.predict(obs)[0]
         obs, reward, done, info = vec_env.step(action)
-        from PIL import Image
-
-        image = Image.fromarray(obs[0].reshape((96,96)), "L")
-        if t > 5 and t < 12:
-            #image.convert("L").save("test.bmp")
-            #mage.show()
-            pass
-        t += 1
         if done:
             game_count += 1
             vec_env.reset()
@@ -222,7 +246,7 @@ def play():
 
 
 if __name__ == "__main__":
-
-    #train(timesteps=30000000, env_count=4)
-    #play()
-    print("Hello")
+    if args.train:
+        train()
+    elif args.play:
+        play()
